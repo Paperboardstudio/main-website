d1fc859e11b0b6492087d88a260ac6c5dd675420
miguelfrnandez <76632459+miguelfrnandez@users.noreply.github.com>
2025-02-10 18:47:39 -0500

    Update: Transition page visual update

--- src/pages/TransitionPage/TransitionPage.jsx
+++ src/pages/TransitionPage/TransitionPage.jsx
@@ -8,9 +8,16 @@ export default function TransitionPage() {
     <div className="transition-page">
       <Canvas gl={{ antialias: false }} className="canvas">
         <Preload />
-        <Html style={{ width: '100%' }}> {/* Removed center prop */}
+        <Html
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
           <div className="transition-message">
-            We provide entrepreneurs and companies around the world
+          We provide entrepreneurs and companies around the world with efficient and innovative tools. Leveraging cutting-edge technology and creativity, we are committed to creating solutions that deliver results for our clients.
           </div>
         </Html>
       </Canvas>
